f792b46e004d931a0663a553f1da5367c3590f2e
MarquesMat <90152478+MarquesMat@users.noreply.github.com>
2023-06-28 14:14:29 -0300

    Update renomear_arq.sh

--- renomear_arq.sh
+++ renomear_arq.sh
@@ -2,41 +2,49 @@
 
 # Definição do caminho base
 # caminho_base="/var/cache/zoneminder/events/"
-caminho_base="/home/monitora/Documentos/projeto_monitora/events/"
+caminho_base="/home/monitora/Documentos/projeto_monitora/events_copy/"
 
-# Encontrar todas as pastas dentro do caminho base
+# Encontrar todas as pastas dentro do caminho base --> monitores
 pastas=$(find "$caminho_base" -mindepth 1 -maxdepth 1 -type d)
 
 for pasta in $pastas; do
-    # Encontrar todas as subpastas dentro da pasta atual
+    # Encontrar todas as subpastas dentro da pasta atual --> dias
     subpastas=$(find "$pasta" -mindepth 1 -maxdepth 1 -type d)
 
     for sub in $subpastas; do
-	subpasta="${sub}/"
+	#subpasta="${sub}/"
+	# Encontrar todas as subpastas dentro da subpasta atual --> eventos
+	subsubpastas=$(find "$sub/" -mindepth 1 -maxdepth 1 -type d)
+	
+	for subsub in $subsubpastas; do
+		
+		# Encontrar todos os arquivos dentro da subsubpasta atual --> Vídeos e fotos
+		arquivos=$(find "$subsub/" -maxdepth 1 -type f)
 
-	# Encontrar todos os arquivos dentro da subpasta atual
-	arquivos=$(find "$subpasta" -maxdepth 1 -type f)
+		# Loop pelos arquivos encontrados
+		for arquivo in $arquivos; do
+			# Verificar se o arquivo é um vídeo no formato MP4
+                	if file --mime-type "$arquivo" | grep -q "video/mp4"; then
+				# Obter o nome do arquivo
+				# nome_arquivo=$(basename "$arquivo")
 
-	# Loop pelos arquivos encontrados
-	for arquivo in $arquivos; do
-		# Obter o nome do arquivo
-		nome_arquivo=$(basename "$arquivo")
+				# Obter o horário de criação do arquivo
+				time=$(stat -c %Y "$arquivo")
 
-		# Obter o horário de criação do arquivo
-		time=$(stat -c %Y "$arquivo")
+				# Converter o horário de criação em formato legível
+				time_formatado=$(date -d @"$time" "+%Y%m%d_%H%M%S")
 
-		# Converter o horário de criação em formato legível
-		time_formatado=$(date -d @"$time" "+%Y%m%d_%H%M%S")
+				# Construir o novo nome do arquivo
+				novo_nome="${pasta}/$(basename "$pasta")_${time_formatado}"
 
-		# Construir o novo nome do arquivo
-		novo_nome="${pasta}/$(basename "$pasta")_${time_formatado}"
+				# Renomear o arquivo
+				mv "$arquivo" "$novo_nome"
 
-		# Renomear o arquivo
-		mv "$arquivo" "$novo_nome"
-
-		# Exibir informações sobre a renomeação
-		echo "Arquivo renomeado: $nome_arquivo -> $(basename "$novo_nome")"
-		echo "Horário de criação: $time_formatado"
-    	done
+				# Exibir informações sobre a renomeação
+				# echo "Arquivo renomeado: $nome_arquivo -> $(basename "$novo_nome")"
+				# echo "Horário de criação: $time_formatado"
+			fi
+	    	done
+	done
     done
 done
